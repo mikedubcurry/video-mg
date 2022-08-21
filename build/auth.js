b2334a27c4c1f7166e4a81a690dada1cae4288db
mikedubcurry <michaelcurry95@gmail.com>
2022-08-21 13:24:59 -0400

    no build

--- build/auth.js
+++
@@ -1,7 +0,0 @@
-import users from "./users.json" assert { type: "json" };
-export function checkAuth(username, password) {
-    return (users.filter((user) => {
-        console.log(user, username, password);
-        return user.username === username && user.password === password;
-    }).length === 1);
-}
